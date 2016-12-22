71b2095255679d5c36206b36ad3e1ccdf2fc6f60
guilherme <guilherme@gac.guilherme>
2016-12-22 02:24:34 -0200

    Double Linked Dynamic List

---
+++ Proj3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lista-din-dup-enc.h"
+
+
+int main(){
+
+	Lista *L1 = definir();
+	Celula *elem1, *elem2, *elem3, *elem4; 
+
+	elem1 = aloca_cel(50,"guilherme");
+	elem2 = aloca_cel(37,"carlos");
+	elem3 = aloca_cel(88,"fabio");
+	elem4 = aloca_cel(13,"anderson");	
+
+	return 0;
+}
